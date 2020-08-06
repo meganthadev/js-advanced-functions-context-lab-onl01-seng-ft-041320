f730d61334cbe62966c7df550c8dfd4bd62bee37
Megan Horsfield <meganthadev@gmail.com>
2020-08-06 06:02:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ let allWagesFor = function () {
 
 function createEmployeeRecord(array) {
   return {
-    
+       firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
 }
