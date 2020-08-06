db8141ea05ef6db0d1f7655753fc4623bb7792d5
Megan Horsfield <meganthadev@gmail.com>
2020-08-06 06:01:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-
-
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +9,5 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function 
